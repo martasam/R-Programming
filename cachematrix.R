b986a722d0064e791abb0eb8d428e1bafa1ed139
martasam <marta.incognito@gmail.com>
2014-05-25 00:38:34 +0200

    final change
    This is the final version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## This code is used to save inversion of matrix in enviroment if it was once calculated.
+## It speeds running of programm up, if there is a need to use inverse of matrix
+## more than once.
+
 ## Write a short comment describing this function
+
+## This function is creating a list to store functions, which we can use on matrix
+## (set, get, setInverse and getInverse).
+## With this function we can store and get matrix and its inverse in different enviroment.
 
 makeCacheMatrix <- function(x = matrix()) {
 	i <- NULL
@@ -10,7 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
 		i <<- NULL
 	}
 	get <- function() x
-	setInverse <- function(solve) i <<- solve
+	setInverse <- function(inverse) i <<- inverse
 	getInverse <- function() i
 	list(set = set, get = get,
 		setInverse = setInverse,
@@ -19,6 +27,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## This functions check whether inverse of this matrix was already calculated
+## and is stored in enviroment.
+## If yes, it will return this inverse, otherwise it will calculate inverse
+## and set this inverse in enviroment.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
